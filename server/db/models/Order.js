bcb9ef2bbc3af6ad2d5f3e24a1f70e85c6e5b95d
edgarc0998 <edgarc0998@gmail.com>
2019-07-31 11:18:35 -0500

    checking if a cart has both userid and sid

--- server/db/models/Order.js
+++ server/db/models/Order.js
@@ -5,16 +5,19 @@ const Order = db.define('order', {
   status: {
     type: Sequelize.ENUM('Created', 'Processing', 'Cancelled', 'Completed')
   },
-//removed sessionId as it is now added as 'sid' via association with session table
+  //removed sessionId as it is now added as 'sid' via association with session table
   subtotal: {
     type: Sequelize.INTEGER
   }
 })
 
 Order.beforeValidate(order => {
-  if (!order.sessionId && !order.userId) {
+  if (!order.sid && !order.userId) {
     const err = new Error('a sessionId or userId is required')
     throw err
+  }
+  if (order.userId) {
+    order.sid = null
   }
 })
 
