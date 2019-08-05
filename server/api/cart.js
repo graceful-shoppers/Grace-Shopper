d66e863ce6548c13050962d6675a7b4c7b3a9123
edgarc0998 <edgarc0998@gmail.com>
2019-08-05 13:03:54 -0500

    merge

--- server/api/cart.js
+++ server/api/cart.js
@@ -68,10 +68,6 @@ router.post('/', async (req, res, next) => {
 
 router.get('/', async (req, res, next) => {
   try {
-    console.log('cart is', req.cart)
-    console.log('session is', req.sessionID)
-    console.log('user is', req.user)
-
     res.json(req.cart)
   } catch (err) {
     next(err)
