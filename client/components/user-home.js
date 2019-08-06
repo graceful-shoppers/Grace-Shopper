fea246d104f3adbb732863b0e0c234a1f87b5640
Ben Jenkins <ben.jenkins@icloud.com>
2019-08-06 09:42:09 -0500

    SHOVELKNIGHT

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import styled from 'styled-components'
 
+const Image = styled.img`
+max-height: 100vh
+max-width: 100vw`
 /**
  * COMPONENT
  */
@@ -11,6 +15,7 @@ export const UserHome = props => {
   return (
     <div>
       <h3>Welcome, {email}</h3>
+      <Image src="https://yachtclubgames.com/wp-content/uploads/2017/03/shovelKnight007.png" />
     </div>
   )
 }
