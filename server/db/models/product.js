32b7f0c338ac0068d2b20f3a1bc05114e2d81d8d
tom.sinovich@gmail.com <tomsinovich@gmail.com>
2019-08-05 20:28:02 -0500

    adds brand field to product model

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -50,6 +50,10 @@ const Product = db.define('product', {
     //   defaultValue:
     //     'https://gloimg.gbtcdn.com/soa/gb/pdm-provider-img/straight-product-img/20180423/T019096/T0190960279/goods_img_big-v1/130754-5523.jpg'
     // }
+  },
+  brand: {
+    type: Sequelize.STRING,
+    allowNull: true
   }
 })
 
